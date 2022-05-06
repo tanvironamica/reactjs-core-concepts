93a1668c216d33428d401f9f8bc468426a80ddf0
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-05-06 18:25:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number =555;
+const singers = [
+  {name: 'Dr. Mahfuz', job: 'Singer'},
+ {name: 'Eva Rahman', job: 'Singer2'},
+ {name: 'Agun', job: 'sopno'},
+ {name: 'shuvro', job: 'pathor'}
+]
+
+const singerStyle= {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+
+
 function App() {
+  const nayoks = ['Rubel', 'BappaRaz', 'Kuber', 'Joshim', 'Salman Shah', 'Riyaz', 'Anwar'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+     {/* <Person name={nayoks[0]} nayika="moushumi"></Person>
+     <Person name={nayoks[1]} nayika="cheka"></Person>
+     <Person name={nayoks[2]} nayika="Kopila"></Person> */}
+     
+     <h5>New component. YAY</h5>
+     <p id="totocompany">rock mama React mama.</p>
+     <Friend movie="Shingam" phone="01777"></Friend>
+     <Friend phone="01999"></Friend>
+    
     </div>
   );
 }
 
+function Person(props){
+  // console.log(props);
+  return (
+    <div className="person">
+    <h1>{props.name}</h1>
+    <p> {props.nayika}</p>
+  </div>
+  )
+}
+
+function Friend(props){
+  // console.log(props);
+  return (
+    <div className='container'>
+      <h3>Name: {props.movie}</h3>
+      <p>phone: {props.phone}</p>
+    </div>
+  )
+}
 export default App;
